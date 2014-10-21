f7a3b9d6a284878adebb2f6f6290eaaca9d45843
Valerio Lupi <valerio.lupi@te4i.com>
2014-10-21 19:42:05 +0200

    . fixed code when running outside of the ide

--- src/com/valerino/fronteend/Settings.java
+++ src/com/valerino/fronteend/Settings.java
@@ -7,6 +7,11 @@ import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * the application settings
@@ -116,20 +121,52 @@ public class Settings {
      * dump emulator definitions to home folder
      */
     private void dumpEmuDefs() {
-        File srcFolder = new File (this.getClass().getResource("emudefs").getFile());
-        File[] emus = srcFolder.listFiles();
-        for (File f : emus) {
-            File n = new File (_emuDefsFolder, f.getName());
-            try {
-                // dump definitions (without overwriting)
-                Utils.copyFile(f,n,false);
-            } catch (IOException e) {
-                e.printStackTrace();
+        JarFile jf = null;
+        try {
+            jf = new JarFile(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+        } catch (IOException e) {
+            // this happens when called from ide
+            File srcFolder = new File (this.getClass().getResource("emudefs").getFile());
+            File[] emus = srcFolder.listFiles();
+            for (File f : emus) {
+                File n = new File (_emuDefsFolder, f.getName());
+                try {
+                    // dump definitions (without overwriting)
+                    Utils.copyFile(f,n,false);
+                } catch (IOException ee) {
+                    e.printStackTrace();
+                    return;
+                }
             }
+            return;
+        }
+
+        // when run fron jar, extract from jar
+        final Enumeration<JarEntry> entries = jf.entries();
+        while(entries.hasMoreElements()) {
+            final String entryName = entries.nextElement().getName().toLowerCase();
+            if (entryName.endsWith(".json") && entryName.contains("/emudefs/")) {
+                // dump to folder
+                File src = new File (entryName);
+                InputStream is = this.getClass().getResourceAsStream("/com/valerino/fronteend/emudefs/" + src.getName());
+                File tgt = new File (Settings.getInstance()._emuDefsFolder, src.getName());
+                try {
+                    if (!tgt.exists()) {
+                        Files.copy(is,tgt.toPath());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try {
+            jf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    /**
+    /** q
      * initialize settings
      * @throws IOException
      */
